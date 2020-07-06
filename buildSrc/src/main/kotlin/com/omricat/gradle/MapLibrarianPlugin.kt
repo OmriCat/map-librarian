a885f6a29f206ebbf954de17940eadbc2701d496
Joseph Cooper <cooper.joseph@gmail.com>
2020-07-06 10:44:48 +0100

    Studio project files

---
+++ buildSrc/src/main/kotlin/com/omricat/gradle/MapLibrarianPlugin.kt
@@ -0,0 +1,33 @@
+package com.omricat.gradle
+
+import com.android.build.gradle.BaseExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.findByType
+import org.gradle.kotlin.dsl.get
+import org.jetbrains.kotlin.gradle.plugin.KotlinBasePluginWrapper
+
+@Suppress("unused")
+class MapLibrarianPlugin : Plugin<Project> {
+    override fun apply(project: Project) = with(project) {
+        if (hasKotlinPlugin())
+            configureAndroid()
+            configureKotlin()
+        }
+
+    private fun Project.configureKotlin() {
+        // NOOP
+    }
+
+    private fun Project.configureAndroid() {
+        val sourceSetsToConfigure = listOf("main", "test", "debug", "release")
+        extensions.findByType<BaseExtension>()?.apply {
+            sourceSetsToConfigure.forEach { srcSet ->
+                sourceSets[srcSet].java.srcDir("/src/$srcSet/kotlin")
+            }
+        }
+    }
+}
+
+private fun Project.hasKotlinPlugin(): Boolean =
+    plugins.asSequence().mapNotNull { it as? KotlinBasePluginWrapper }.firstOrNull() != null
