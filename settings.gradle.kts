c99c96f69b806b7e9521b7b8cadd1b958b55ffd1
Joseph Cooper <cooper.joseph@gmail.com>
2021-09-08 11:58:36 +0100

    Bump square/workflow to rc2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,8 @@ pluginManagement {
 
 plugins {
     id("de.fayard.refreshVersions") version "0.11.0"
+////                            # available:"0.20.0"
+////                            # available:"0.21.0"
 }
 
 rootProject.name = ("map-librarian")
