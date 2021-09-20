b7473b8afadb6f421d92baa85878314b0c308d8f
Joseph Cooper <cooper.joseph@gmail.com>
2021-09-20 13:00:52 +0100

    Move Workers util methods to :core module

--- core/src/main/kotlin/com/omricat/workflow/Workers.kt
+++ core/src/main/kotlin/com/omricat/workflow/Workers.kt
@@ -7,7 +7,7 @@ import com.squareup.workflow1.asWorker
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 
-fun <V, E> resultWorker(
+public fun <V, E> resultWorker(
     exceptionWrapper: (Throwable) -> E,
     body: suspend () -> Result<V, E>
 ): Worker<Result<V, E>> =
