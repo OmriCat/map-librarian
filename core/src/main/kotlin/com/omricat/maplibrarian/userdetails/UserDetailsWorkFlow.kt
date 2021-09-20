f4fd8ad2c0cd1865689ced38b010fd06287aeb30
Joseph Cooper <cooper.joseph@gmail.com>
2021-09-20 14:06:40 +0100

    Move UserDetailsWorkflow to :core module

--- core/src/main/kotlin/com/omricat/maplibrarian/userdetails/UserDetailsWorkFlow.kt
+++ core/src/main/kotlin/com/omricat/maplibrarian/userdetails/UserDetailsWorkFlow.kt
@@ -3,9 +3,9 @@ package com.omricat.maplibrarian.userdetails
 import com.omricat.maplibrarian.model.User
 import com.squareup.workflow1.StatelessWorkflow
 
-object UserDetailsWorkFlow :
+public object UserDetailsWorkFlow :
     StatelessWorkflow<User, UserDetailsWorkFlow.LogOut, UserDetailsScreen>() {
-    object LogOut
+    public object LogOut
 
     override fun render(renderProps: User, context: RenderContext): UserDetailsScreen =
         UserDetailsScreen(
@@ -14,7 +14,7 @@ object UserDetailsWorkFlow :
         )
 }
 
-data class UserDetailsScreen(
+public data class UserDetailsScreen(
     val user: User,
     val onLogOutClicked: () -> Unit
 )
