f1c20c6bf3ff22465a594f5c13adbe64fd6ef353
Joseph Cooper <cooper.joseph@gmail.com>
2021-09-29 17:49:40 +0100

    index on uncancellable-coroutines: 58e9890 Introduce runSuspendCatching which allows coroutines started within it to be cancelled if the parent scope is cancelled

---
+++ core/src/main/kotlin/com/omricat/maplibrarian/utils/DispatcherProvider.kt
@@ -0,0 +1,19 @@
+package com.omricat.maplibrarian.utils
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+public interface DispatcherProvider {
+
+    public val default: CoroutineDispatcher
+    public val io: CoroutineDispatcher
+    public val main: CoroutineDispatcher
+    public val unconfined: CoroutineDispatcher
+
+    public companion object Default : DispatcherProvider {
+        override val default: CoroutineDispatcher get() = Dispatchers.Default
+        override val io: CoroutineDispatcher get() = Dispatchers.IO
+        override val main: CoroutineDispatcher get() = Dispatchers.Main
+        override val unconfined: CoroutineDispatcher get() = Dispatchers.Unconfined
+    }
+}
