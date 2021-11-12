0b78e14d469195bf3ef8da478df8c00db051f2ff
Joseph Cooper <cooper.joseph@gmail.com>
2021-11-12 17:41:36 +0000

    Add constructor function to AndroidBuildToolVersions in buildSrc

--- buildSrc/src/main/kotlin/com/omricat/gradle/AndroidBuildToolVersions.kt
+++ buildSrc/src/main/kotlin/com/omricat/gradle/AndroidBuildToolVersions.kt
@@ -4,4 +4,16 @@ interface AndroidBuildToolVersions {
     val compileSdk: Int
     val minSdk: Int
     val targetSdk: Int
+
+    companion object {
+        operator fun invoke(
+            compileSdk: Int,
+            minSdk: Int,
+            targetSdk: Int
+        ): AndroidBuildToolVersions = object : AndroidBuildToolVersions {
+            override val compileSdk: Int = compileSdk
+            override val minSdk: Int = minSdk
+            override val targetSdk: Int = targetSdk
+        }
+    }
 }
