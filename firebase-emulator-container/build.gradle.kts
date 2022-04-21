6cc9b24364bff4f0dfd428dafdc65b49e7508fcb
Joseph Cooper <cooper.joseph@gmail.com>
2022-04-21 19:03:32 +0100

    build(firebase-emulator-container): Firestore.rules
    Use the root project's firestore.rules in FirebaseEmulatorContainer

--- firebase-emulator-container/build.gradle.kts
+++ firebase-emulator-container/build.gradle.kts
@@ -7,3 +7,7 @@ dependencies {
     api(platform("org.testcontainers:testcontainers-bom:_"))
     api("org.testcontainers:testcontainers")
 }
+
+tasks.processResources {
+    from(rootProject.layout.projectDirectory.file("firestore.rules"))
+}
--- firebase-emulator-container/src/main/kotlin/com/omricat/firebaseemulatorcontainer/FirebaseEmulatorContainer.kt
+++ firebase-emulator-container/src/main/kotlin/com/omricat/firebaseemulatorcontainer/FirebaseEmulatorContainer.kt
@@ -1,19 +1,22 @@
 package com.omricat.firebaseemulatorcontainer
 
+import org.testcontainers.containers.BindMode.READ_ONLY
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.utility.DockerImageName
 
 class FirebaseEmulatorContainer : GenericContainer<FirebaseEmulatorContainer>(
-    DockerImageName.parse("ghcr.io/grodin/firebase-emulator-docker:v1.2.0")
+    DockerImageName.parse("ghcr.io/grodin/firebase-emulator-docker:v$DOCKER_IMAGE_VERSION")
 ) {
     init {
         withExposedPorts(FIRESTORE_PORT, AUTH_PORT)
         waitingFor(Wait.forHealthcheck())
+        withClasspathResourceMapping("firestore.rules", "./firestore.rules", READ_ONLY)
         withCommand("emulators:start")
     }
 
     companion object {
+        const val DOCKER_IMAGE_VERSION = "1.2.0"
         const val FIRESTORE_PORT = 8080
         const val AUTH_PORT = 9099
     }
