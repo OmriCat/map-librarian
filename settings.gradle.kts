9eaf0e9bfe997d126a66352a8fcaab69fda06844
Joseph Cooper <cooper.joseph@gmail.com>
2023-07-20 17:57:47 +0100

    build: Remove refreshVersions gradle plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,7 +24,6 @@ dependencyResolutionManagement {
 
 plugins {
     id("com.gradle.enterprise") version "3.12.2"
-    id("de.fayard.refreshVersions") version "0.51.0"
     id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.1.5"
 }
 
@@ -33,10 +32,6 @@ gradleEnterprise {
         termsOfServiceUrl = "https://gradle.com/terms-of-service"
         termsOfServiceAgree = "yes"
     }
-}
-
-refreshVersions {
-    rejectVersionIf { candidate.stabilityLevel.isLessStableThan(current.stabilityLevel) }
 }
 
 gitHooks {
