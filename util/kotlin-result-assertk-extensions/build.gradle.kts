91e2e4db4d3c03d2bbe98d69f550fd11d2f3082c
Joseph Cooper <cooper.joseph@gmail.com>
2023-07-24 15:56:02 +0100

    test: Add AssertK asserts for kotlin-result

--- util/kotlin-result-assertk-extensions/build.gradle.kts
+++ util/kotlin-result-assertk-extensions/build.gradle.kts
@@ -3,6 +3,7 @@ plugins { alias(libs.plugins.maplib.kotlin.library) }
 dependencies {
     api(libs.kotlinResult)
     api(libs.kotest.assertions.core)
+    api(libs.assertk)
 }
 
 kotlin { sourceSets.all { languageSettings.optIn("kotlin.contracts.ExperimentalContracts") } }
