57fab71ae43084de5712467b7c61c594d9af20d6
Joseph Cooper <cooper.joseph@gmail.com>
2023-08-03 15:09:47 +0100

    feat: Introduce Loggable convenience interface
    Uses context receivers to make logging easy in classes with injected
    loggers. Inline functions compute a tag automatically.

--- util/logging/build.gradle.kts
+++ util/logging/build.gradle.kts
@@ -2,3 +2,5 @@
 plugins { alias(libs.plugins.maplib.kotlin.library) }
 
 dependencies { api(libs.kermit.core) }
+
+kotlin { compilerOptions { freeCompilerArgs.addAll("-Xcontext-receivers") } }
---
+++ util/logging/src/main/kotlin/com/omricat/logging/Loggable.kt
@@ -0,0 +1,42 @@
+package com.omricat.logging
+
+import co.touchlab.kermit.Severity
+import co.touchlab.kermit.Severity.Debug
+import co.touchlab.kermit.Tag
+import kotlin.reflect.KClass
+
+public interface Loggable {
+    public val logger: Logger
+}
+
+context(Loggable)
+
+public inline fun <reified T : Any> T.log(
+    priority: Severity = Debug,
+    tag: String? = null,
+    noinline message: () -> String
+) {
+    logger.log(priority, Tag(tag ?: T::class.outerClassSimpleName()), message)
+}
+
+context(Loggable)
+
+public inline fun <reified T : Any> T.log(
+    priority: Severity = Debug,
+    tag: String? = null,
+    throwable: Throwable,
+    noinline message: () -> String
+) {
+    logger.log(priority, Tag(tag ?: T::class.outerClassSimpleName()), throwable, message)
+}
+
+public fun KClass<*>.outerClassSimpleName(): String {
+    val fullClassName = this.java.name
+    val outerClassName = fullClassName.substringBefore('$')
+    val simplerOuterClassName = outerClassName.substringAfterLast('.')
+    return if (simplerOuterClassName.isEmpty()) {
+        fullClassName
+    } else {
+        simplerOuterClassName.removeSuffix("Kt")
+    }
+}
