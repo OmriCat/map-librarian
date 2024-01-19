7c9b2d598d7e3e70d6fd7f4a31f65e309f19ee02
Joseph Cooper <grodin@users.noreply.github.com>
2024-01-19 18:58:45 +0000

    fix(build-logic): use correct gradle property names

--- build-logic/convention-plugins/src/main/kotlin/com/omricat/maplibrarian/gradle/BuildVersions.kt
+++ build-logic/convention-plugins/src/main/kotlin/com/omricat/maplibrarian/gradle/BuildVersions.kt
@@ -9,22 +9,22 @@ internal fun Project.javaLanguageVersionFromGradleProperties(): Provider<JavaLan
     javaVersionFromGradleProperties().map { s -> JavaLanguageVersion.of(s.majorVersion) }
 
 internal fun Project.javaVersionFromGradleProperties(): Provider<JavaVersion> =
-    gradleProperty("javaversion").map { JavaVersion.toVersion(it) }.orElse(JavaVersion.VERSION_11)
+    gradleProperty("com.omricat.maplib.javaVersion").map { JavaVersion.toVersion(it) }.orElse(JavaVersion.VERSION_11)
 
 private const val DEFAULT_COMPILE_SDK = 33
 
 internal fun Project.compileSdkFromGradleProperties(): Provider<Int> =
-    gradleProperty("compilesdk").orElse(DEFAULT_COMPILE_SDK)
+    gradleProperty("com.omricat.maplib.compilesdk").orElse(DEFAULT_COMPILE_SDK)
 
 private const val DEFAULT_MIN_SDK = 23
 
 internal fun Project.minSdkFromGradleProperties(): Provider<Int> =
-    gradleProperty("minSdk").orElse(DEFAULT_MIN_SDK)
+    gradleProperty("com.omricat.maplib.minSdk").orElse(DEFAULT_MIN_SDK)
 
 private const val DEFAULT_TARGET_SDK = 29
 
 internal fun Project.targetSdkFromGradleProperties(): Provider<Int> =
-    gradleProperty("targetSdk").orElse(DEFAULT_TARGET_SDK)
+    gradleProperty("com.omricat.maplib.targetSdk").orElse(DEFAULT_TARGET_SDK)
 
 private fun Project.gradleProperty(id: String): Provider<Int> =
     providers.gradleProperty("com.omricat.maplib.${id}").map { s -> s.toInt() }
