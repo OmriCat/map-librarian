55371111314a71e9e15d6a4ff330a2eb89d9a59e
Joseph Cooper <2599295+grodin@users.noreply.github.com>
2024-01-19 12:57:57 +0000

    fix: Migrate from deprecated Detekt API

--- build-logic/convention-plugins/src/main/kotlin/DetektConventionPlugin.kt
+++ build-logic/convention-plugins/src/main/kotlin/DetektConventionPlugin.kt
@@ -1,19 +1,24 @@
 import io.gitlab.arturbosch.detekt.DetektPlugin
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+import io.gitlab.arturbosch.detekt.Detekt
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.withType
+import java.io.File
 
 internal class DetektConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             pluginManager.apply(DetektPlugin::class)
             configure<DetektExtension> {
-                config = rootProject.files(DETEKT_CONFIG_FILE)
-                reports.html {
-                    enabled = true
-                    destination = file("build/reports/detekt.html")
+                config.setFrom(rootProject.files(DETEKT_CONFIG_FILE))
+            }
+            tasks.withType<Detekt>().configureEach { task ->
+                task.reports.html {
+                    it.required.set(true)
+                    it.outputLocation.set(rootProject.layout.buildDirectory.file("reports/detekt.html"))
                 }
             }
         }
