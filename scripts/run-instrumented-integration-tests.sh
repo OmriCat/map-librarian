6f0668b18b6b36c7ffad00cd57ea02e8916c3be2
Joseph Cooper <grodin@users.noreply.github.com>
2024-01-22 22:26:08 +0000

    Improve integration tests (#270)
    Co-authored-by: Joseph Cooper <2599295+grodin@users.noreply.github.com>

--- scripts/run-instrumented-integration-tests.sh
+++ scripts/run-instrumented-integration-tests.sh
@@ -1,3 +1,39 @@
 #!/bin/bash
-set -o errexit -o nounset
-adb shell am instrument -w com.omricat.maplibrarian.integrationtesting.debug/androidx.test.runner.AndroidJUnitRunner
+
+set -o nounset
+
+bail() {
+  local err_code=$1
+  shift
+  echo "Error: $*"
+  exit "$err_code"
+}
+
+project_dir="$(git rev-parse --show-toplevel)"
+
+echo "Clearing logcat logs"
+adb logcat -c
+
+logcat_dir="$project_dir/build/logs"
+mkdir -p "$logcat_dir" || bail 11 "Couldn't create directory $logcat_dir"
+
+logcat_logfile="$logcat_dir/logcat.log"
+touch "$logcat_logfile"
+
+echo "Logcat being written to $logcat_logfile"
+adb logcat './*:D' > "$logcat_logfile" &
+logcat_pid="$!"
+
+exit-kill-logcat() {
+  echo "Killing adb logcat process with PID $logcat_pid"
+  kill -TERM "$logcat_pid"
+  exit "$1"
+}
+
+trap 'exit-kill-logcat 20' INT TERM
+
+adb_cmd="adb shell am instrument -w com.omricat.maplibrarian.integrationtesting.debug/androidx.test.runner.AndroidJUnitRunner"
+echo "Executing command $adb_cmd in project dir $project_dir"
+cd "$project_dir" || bail 10 "Couldn't cd into $project_dir"
+$adb_cmd
+exit-kill-logcat "$?"
