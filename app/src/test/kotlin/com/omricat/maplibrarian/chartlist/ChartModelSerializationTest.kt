74c9b11c84c54c43482300a27f585ffa6b71d8d7
Joseph Cooper <grodin@users.noreply.github.com>
2024-02-12 18:35:56 +0000

    refactor: Improve tests of Chart map serialization (#295)
    Co-authored-by: Joseph Cooper <2599295+grodin@users.noreply.github.com>

---
+++ app/src/test/kotlin/com/omricat/maplibrarian/chartlist/ChartModelSerializationTest.kt
@@ -0,0 +1,45 @@
+package com.omricat.maplibrarian.chartlist
+
+import assertk.assertAll
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isInstanceOf
+import com.omricat.maplibrarian.chartlist.DeserializerError.PropertyNonFoundError
+import com.omricat.maplibrarian.model.UnsavedChartModel
+import com.omricat.maplibrarian.model.UserUid
+import com.omricat.result.assertk.isErr
+import com.omricat.result.assertk.isOk
+import kotlin.test.Test
+
+internal class ChartModelSerializationTest {
+
+    @Test
+    fun `serializing then deserializing should be identity`() {
+        val chartModel = UnsavedChartModel(UserUid("userId"), "Chart title")
+        val result =
+            ChartModelFromMapDeserializer.deserializeFromMap(
+                ChartModelToMapSerializer.serializeToMap(chartModel)
+            )
+        assertThat(result).isOk().isEqualTo(chartModel)
+    }
+
+    @Test
+    fun `deserializing should fail if any property is missing`() {
+        val serialized: Map<String, Any> =
+            ChartModelToMapSerializer.serializeToMap(
+                UnsavedChartModel(UserUid("userId"), "Chart title")
+            )
+        val id = "map1"
+
+        val deserializer = ChartModelFromMapDeserializer
+
+        assertAll {
+            serialized
+                .map { (key, _) -> serialized.toMutableMap().minus(key) }
+                .forEach { map ->
+                    val result = deserializer.deserializeFromMap(map)
+                    assertThat(result).isErr().isInstanceOf<PropertyNonFoundError>()
+                }
+        }
+    }
+}
