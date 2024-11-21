d128788783068160e51ea5b15b4403d591fa437e
Joseph Cooper <2599295+grodin@users.noreply.github.com>
2024-11-21 21:24:15 +0000

    build: Migrate gradle enterprise plugin (#431)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,15 +23,15 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("com.gradle.enterprise") version "3.18.2"
+    id("com.gradle.develocity") version "3.18.2"
     id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.15"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-gradleEnterprise {
+develocity {
     buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
     }
 }
 
