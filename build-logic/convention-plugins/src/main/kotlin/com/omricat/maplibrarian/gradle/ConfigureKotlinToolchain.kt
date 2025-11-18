049ff3d29521df38e38fdf12c39bead051523a36
Joseph Cooper <cooper.joseph@gmail.com>
2025-11-18 13:22:38 +0000

    build: Fix deprecations in build-logic

--- build-logic/convention-plugins/src/main/kotlin/com/omricat/maplibrarian/gradle/ConfigureKotlinToolchain.kt
+++ build-logic/convention-plugins/src/main/kotlin/com/omricat/maplibrarian/gradle/ConfigureKotlinToolchain.kt
@@ -1,9 +1,9 @@
 package com.omricat.maplibrarian.gradle
 
 import org.gradle.api.Project
-import org.jetbrains.kotlin.gradle.dsl.KotlinTopLevelExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinBaseExtension
 
-internal fun Project.configureJavaToolchainForKotlin(kotlinExtension: KotlinTopLevelExtension) =
+internal fun Project.configureJavaToolchainForKotlin(kotlinExtension: KotlinBaseExtension) =
     with(kotlinExtension) {
         jvmToolchain { it.languageVersion.set(javaLanguageVersionFromGradleProperties()) }
     }
